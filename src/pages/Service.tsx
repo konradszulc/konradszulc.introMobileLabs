4ca8343cafc9a65c9de892189794d86df5a5a997
konradszulc <konradszulc321@gmail.com>
2025-09-27 16:52:08 +0300

    Initial Commit

---
+++ src/pages/Service.tsx
@@ -0,0 +1,25 @@
+// pages/Service.tsx
+
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+
+
+const Service: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Service</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Service</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Service;
